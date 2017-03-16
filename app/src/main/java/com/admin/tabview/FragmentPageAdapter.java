b1623e4f7767ace80b3906e6e8b43c8e7849f840
Yash <yagnikprajapati@gmail.com>
2017-03-17 00:36:54 +0530

    Tab View

--- app/src/main/java/com/admin/tabview/FragmentPageAdapter.java
+++ app/src/main/java/com/admin/tabview/FragmentPageAdapter.java
@@ -4,11 +4,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+
 /**
  * Created by admin on 3/16/2017.
  */
 
 public class FragmentPageAdapter extends FragmentPagerAdapter {
+    
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> fragmentTitle = new ArrayList<>();
+    
+    
 
     public FragmentPageAdapter(FragmentManager fm) {
         super(fm);
@@ -16,14 +23,19 @@ public class FragmentPageAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return fragments.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return fragments.size();
     }
 
+    public void addFragment(Fragment fragment, String title){
+        fragments.add(fragment);
+        fragmentTitle.add(title);
+    }
+    
     @Override
     public CharSequence getPageTitle(int position) {
         return super.getPageTitle(position);
--- app/src/main/java/com/admin/tabview/MainActivity.java
+++ app/src/main/java/com/admin/tabview/MainActivity.java
@@ -1,15 +1,34 @@
 package com.admin.tabview;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        FragmentPageAdapter fragmentPageAdapter = new FragmentPageAdapter(getSupportFragmentManager());
+        fragmentPageAdapter.addFragment(new Appliences(),"Music");
+        fragmentPageAdapter.addFragment(new Books(),"Books");
+        fragmentPageAdapter.addFragment(new Wearable(),"Wear");
+
+        viewPager.setAdapter(fragmentPageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
